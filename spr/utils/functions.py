24aad747f8fd54f330d47c447a7cde89b49065d3
It's Hussain <90657846+The-Death-Soul@users.noreply.github.com>
2022-01-09 20:05:23 +0530

    Create functions.py

--- spr/utils/functions.py
+++ spr/utils/functions.py
@@ -105,7 +105,7 @@ __Message has been deleted__
         {
             "Correct (0)": "upvote_spam",
             "Incorrect (0)": "downvote_spam",
-            "Chat": "https://t.me/" + (message.chat.username or "SpamProtectionLog/93"),
+            "Chat": "https://t.me/" + (message.chat.username or "Death_Soul_fedlogs"),
         },
         2
     )
